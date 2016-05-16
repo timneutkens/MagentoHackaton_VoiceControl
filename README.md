813102c4a51878cd5603f296ec2bf4342909f3ee
tim neutkens <tim@weprovide.com>
2016-05-16 19:18:04 +0200

    English as primary language

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Magento 2 VoiceControl
 Control the Magento frontend with your voice
+
+To trigger the voice control use: `ctrl + m`
+
+Available commands:
+
+* Go to homepage
+* Next page
+* Previous page
+* Search for <what you want to search for>
+
+Webshops usually don't get to talk that much. So maybe ask how the Magento is doing..?
--- view/frontend/web/VoiceControl.js
+++ view/frontend/web/VoiceControl.js
@@ -1,36 +1,58 @@
-define([], function() {
+define([], function () {
   'use strict';
+  if (typeof webkitSpeechRecognition === 'undefined' && typeof speechSynthesis !== 'undefined') {
+    return;
+  }
+
+  function triggerNavigationClick (el) {
+    elements = document.getElementsByClassName('action ' + el);
+    if (elements.length > 0) {
+      elements[1].click();
+    }
+  }
+
   var recognition = new webkitSpeechRecognition();
   var elements;
-  recognition.lang = 'nl-NL';
+  recognition.lang = 'en-US';
   recognition.onresult = function (event) {
     var text = event.results[0][0].transcript;
+
     console.log(text);
-    if (text.indexOf('volgende pagina') !== -1) {
-      elements = document.getElementsByClassName('action next');
-      elements[1].click();
+
+    if (text.indexOf('go to homepage') !== -1) {
+      location.href = '/';
       return;
     }
 
-    if (text.indexOf('vorige pagina') !== -1) {
-      elements = document.getElementsByClassName('action previous');
-      elements[1].click();
+    if (text.indexOf('next page') !== -1) {
+      triggerNavigationClick('next');
       return;
     }
 
-    if (text.indexOf('zoek') !== -1) {
-      text = text.replace('zoek ', '');
+    if (text.indexOf('previous page') !== -1) {
+      triggerNavigationClick('previous');
+      return;
+    }
+
+    if (text.indexOf('search for') !== -1) {
+      text = text.replace('search for ', '');
       document.getElementById('search').value = text;
       document.getElementById('search_mini_form').submit();
     }
 
-    if(text.indexOf('hoe gaat het') !== -1) {
+    if (text.indexOf('how are you') !== -1) {
       var msg = new SpeechSynthesisUtterance();
       var voices = window.speechSynthesis.getVoices();
-      msg.voice = voices[76];
-      msg.text = 'goed hoor met jou';
-      msg.lang = 'nl-NL';
+      msg.voice = voices[0]; // 76 is dutch
+      msg.text = 'I am fine. What about you';
+      msg.lang = 'en-US';
       speechSynthesis.speak(msg);
     }
   };
+
+  document.addEventListener('keydown', function (event) {
+    if (event.code === 'KeyM' && event.ctrlKey) {
+      recognition.start();
+    }
+  });
 });
